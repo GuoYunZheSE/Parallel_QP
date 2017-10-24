e5e36df1cd6ee391ec08601f630869f974eac648
GuoYunZheSE <guoyunzhe.SE@gmail.com>
2017-10-24 20:11:56 +0800

    10/24

--- Parallel_QP/RPC_Version/Run.py
+++ Parallel_QP/RPC_Version/Run.py
@@ -1,3 +1,4 @@
+
 if __name__ == '__main__':
     import Proxy
     from multiprocessing.connection import Client
--- Parallel_QP/RPC_Version/Server.py
+++ Parallel_QP/RPC_Version/Server.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Server.py
-
+import subprocess
 from multiprocessing.connection import Listener
 from threading import Thread
 
@@ -27,7 +27,13 @@ if __name__ == '__main__':
             cnt += 1
         return cnt
 
-
+    def RunQP():
+        pname=r'C:\Users\user\Documents\Visual Studio 2017\Projects\Print\Debug\Print.exe'
+        #source=bytes(parameter,encoding='utf-8')
+        p = subprocess.Popen(['C:/Users/user/Documents/Visual Studio 2017/Projects/Print/Debug/Print.exe','51','2','2','232'],shell=True,stdout=subprocess.PIPE)
+        result=p.communicate()[0]
+        print(result)
+        return result
     # 新建一个handler类实例, 并将add, printdict方法注册到handler里面
     from Handler import RPCHandler
     rpc_handler = RPCHandler()
@@ -35,4 +41,5 @@ if __name__ == '__main__':
     rpc_handler.register_function(printdict)
 
     # 运行server
-    rpc_server(rpc_handler, ('192.168.2.100', 17000), authkey=bytes('Node_0',encoding='utf8'))
+    RunQP()
+   # rpc_server(rpc_handler, ('192.168.2.100', 17000), authkey=bytes('Node_0',encoding='utf8'))
